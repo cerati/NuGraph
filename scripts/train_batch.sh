df3b878a0089ca71d79eb7eb1c6bc2790849f406
Giuseppe Cerati <cerati@fe01.ds.uchicago.edu>
2026-06-24 11:20:31 -0500

    local changes for DSI cluster setup

--- scripts/train_batch.sh
+++ scripts/train_batch.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#SBATCH -J exatrkx_train
-#SBATCH -t 1440
-#SBATCH -p gpu_gce
+#SBATCH -J nugraph_train
+#SBATCH -t 12:00:00
+#SBATCH -p general
 #SBATCH --gres=gpu:1
-#SBATCH -A fwk
-#SBATCH -q regular
-#SBATCH --cpus-per-task=12
-#SBATCH --signal=SIGUSR1@90
+#SBATCH -q normal
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=64G
 
+source /etc/profile.d/conda.sh
+conda activate /net/projects2/fermi2526/conda/nugraph-25-10
+which python
 srun python scripts/train.py $@
